9acfb116b65fb6940027d5fbcd1b44cadb6a5036
DongSubKim <dskim9218@gmail.com>
2021-04-17 18:04:44 +0900

    Section 27 Exercises 12

---
+++ 27_Exercises_12/01/dog/main.go
@@ -0,0 +1,7 @@
+// Package dog calculates dog years related to human year.
+package dog
+
+// Year multiplies 7 to input to calculate dog year.
+func Year(n int) int {
+	return n * 7
+}
---
+++ 27_Exercises_12/01/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dongsubkim/go-programming/27_Exercises_12/01/dog"
+)
+
+func main() {
+	n := dog.Year(10)
+	fmt.Println("Dog year: ", n)
+}
